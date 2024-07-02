c9347a31ac98077294fd47a4304bae3e2ecc6832
AsandaM <asandamehlo088@gmail.com>
2024-07-02 09:08:48 +0200

    created navbar and footer

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,162 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item" :class="{ active: $route.path === '/' }">
+              <router-link to="/" class="nav-link">
+                <span class="outer">
+                  <span class="inner">HOME</span>
+                </span>
+              </router-link>
+            </li>
+            <li class="nav-item" :class="{ active: $route.path === '/about' }">
+              <router-link to="/about" class="nav-link">
+                <span class="outer">
+                  <span class="inner">ABOUT</span>
+                </span>
+              </router-link>
+            </li>
+            <li class="nav-item" :class="{ active: $route.path === '/resume' }">
+              <router-link to="/resume" class="nav-link">
+                <span class="outer">
+                  <span class="inner">RESUME</span>
+                </span>
+              </router-link>
+            </li>
+            <li class="nav-item" :class="{ active: $route.path === '/projects' }">
+              <router-link to="/projects" class="nav-link">
+                <span class="outer">
+                  <span class="inner">PROJECTS</span>
+                </span>
+              </router-link>
+            </li>
+            <li class="nav-item" :class="{ active: $route.path === '/testimonials' }">
+              <router-link to="/testimonials" class="nav-link">
+                <span class="outer">
+                  <span class="inner">TESTIMONIALS</span>
+                </span>
+              </router-link>
+            </li>
+            <li class="nav-item" :class="{ active: $route.path === '/contact' }">
+              <router-link to="/contact" class="nav-link">
+                <span class="outer">
+                  <span class="inner">CONTACT</span>
+                </span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavBarComp",
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
+    background-color: #d6dbd8;
+    padding: 1rem 2rem;
+  }
+  
+  .navbar-nav {
+    display: flex;
+    justify-content: end;
+    width: 100%;
+  }
+  
+  .nav-item {
+    margin: 0 1rem;
+    position: relative;
+  }
+  
+  .nav-link {
+    text-decoration: none;
+    color: #100f0f;
+    font-size: 20px;
+    position: relative;
+  }
+  
+  .outer::before,
+  .outer::after,
+  .inner::before,
+  .inner::after {
+    content: "";
+    position: absolute;
+    background: #080808;
+    transition: transform 0.6s ease;
+  }
+  
+  .outer::before,
+  .outer::after {
+    height: 1px;
+    width: calc(100% + 12px);
+    top: 0;
+    left: -8px;
+    transform: scale(0);
+  }
+  
+  .outer::after {
+    bottom: 0;
+    transform-origin: right;
+  }
+  
+  .inner::before,
+  .inner::after {
+    width: 1px;
+    height: calc(100% + 10px);
+    top: -5px;
+    transform: scale(0);
+  }
+  
+  .inner::before {
+    left: -2px;
+    transform-origin: bottom;
+  }
+  
+  .inner::after {
+    right: 0;
+    transform-origin: top;
+  }
+  
+  .nav-link:hover .outer::before,
+  .nav-link:hover .outer::after,
+  .nav-link:hover .inner::before,
+  .nav-link:hover .inner::after {
+    transform: scale(1);
+  }
+
+  .nav-item.active .nav-link {
+  position: relative;
+}
+
+.nav-item.active .nav-link::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -2px; /* Adjust the position as needed */
+  width: 100%;
+  height: 2px; /* Adjust the thickness as needed */
+  background: linear-gradient(90deg, #006400, #eeef9d, #afb20a, #7c7d25, #C9CC3F);
+  background-size: 500% 100%;
+  animation: animate-underline 5s linear infinite;
+}
+
+@keyframes animate-underline {
+  0% {
+    background-position: 0% 100%;
+  }
+  100% {
+    background-position: 100% 100%;
+  }
+}
+
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsView from '../views/ProjectsView.vue'
+import ResumeView from '../views/ResumeView.vue'
+import ContactView from '../views/ContactView.vue'
+import TestimonialsView from '../views/TestimonialView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: ProductsView
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: ResumeView
+  },
+  {
+    path: '/testimonials',
+    name: 'testimonials',
+    component: TestimonialsView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
   },
   {
     path: '/about',
